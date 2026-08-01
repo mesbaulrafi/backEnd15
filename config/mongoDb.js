0485660962d89339d95190a034cfdeaa6b747b73
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-08-01 12:34:22 +0600

    otp done

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const mongoDb = ()=>{
-    return mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.e8wo0rw.mongodb.net/${process.env.MONGODB_DBNAME}?appName=Cluster0`)
+    return mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.e8wo0rw.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => console.log('Database Connected successfully'))
     .catch(err =>{
         console.log(err);
