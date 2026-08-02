ef94d3f96eee7bd9b079e289c422421315d63d97
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-08-02 11:22:36 +0600

    remove data base

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -1,12 +1,2 @@
 const mongoose = require('mongoose');
 
-
-const mongoDb = ()=>{
-    return mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.e8wo0rw.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`)
-    .then(() => console.log('Database Connected successfully'))
-    .catch(err =>{
-        console.log(err);
-    })
-}
-
-module.exports = mongoDb
